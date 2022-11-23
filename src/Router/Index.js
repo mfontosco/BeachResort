9914cbfb968af171b7d3c8888215ecb99873d492
mfontosco <59096300+mfontosco@users.noreply.github.com>
2022-11-23 01:22:06 +0100

    made some changes

--- src/Router/Index.js
+++ src/Router/Index.js
@@ -2,10 +2,8 @@ import HomeScreen from "../Screens/HomeScreen";
 import SingleRoomScreen from "../Screens/SingleRoomScreen";
 import RoomsScreen from "../Screens/RoomsScreen";
 import NotFoundScreen from "../Screens/NotFoundScreen";
-// import DashBoardScreen from "../Screens/DashBoardScreen";
 import BookingScreen from '../Screens/BookingScreen'
-import SignupScreen from "../Screens/SignupScreen";
 import LoginScreen from "../Screens/LoginScreen";
 import DashBoardLayoutScreen from "../Screens/DashBoardLayoutScreen";
 
-export {SignupScreen,LoginScreen,HomeScreen,SingleRoomScreen,RoomsScreen,NotFoundScreen,DashBoardLayoutScreen,BookingScreen}
+export {LoginScreen,HomeScreen,SingleRoomScreen,RoomsScreen,NotFoundScreen,DashBoardLayoutScreen,BookingScreen}
--- src/Router/Router.js
+++ src/Router/Router.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Route,Navigate,Routes } from 'react-router-dom'
-import {LoginScreen,SignupScreen,DashBoardLayoutScreen,HomeScreen,SingleRoomScreen,RoomsScreen,NotFoundScreen,BookingScreen} from './Index'
+import {LoginScreen,DashBoardLayoutScreen,HomeScreen,SingleRoomScreen,RoomsScreen,NotFoundScreen,BookingScreen} from './Index'
 
 const Router = () => {
   return (
     <div>
       <Routes>
-        {/* <Route path='/home' element={<HomeScreen/>}/> */}
+        
         <Route path='/rooms' element={<RoomsScreen/>}/>
         <Route path='/rooms/:slug' element={<SingleRoomScreen/>}/>
         <Route path='/notfound' element={<NotFoundScreen/>}/>
