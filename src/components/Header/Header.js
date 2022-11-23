53a7017b33b7aa3d4de3bb073b9ff5b7ce3553c1
mfontosco <59096300+mfontosco@users.noreply.github.com>
2022-11-23 08:08:05 +0100

    made some changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
     {/* <img src={logo} alt='logo' className={styles.img}/> */}
     <div className={styles.logo}><h1>MFON Beach Resort</h1></div>
      <div className={styles.links}>
-      <Link to='/home'>Home</Link>
+      <Link to='/'>Home</Link>
       <Link to='/rooms'>Rooms</Link>
       <Link to='/dashboard'>DashBoard</Link>
       <Link to='/booking'>Booking</Link>
